7a2335f3db8329385700751c59bbea362a7d99ed
Frazhy <franky.quispe.d@uni.pe>
2024-01-20 14:10:00 -0500

    first commit

---
+++ SpringCoreApplication/springmvcboot1/src/main/java/com/telusko/spmvc/Springmvcboot1Application.java
@@ -0,0 +1,13 @@
+package com.telusko.spmvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springmvcboot1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springmvcboot1Application.class, args);
+	}
+
+}
---
+++ SpringCoreApplication/springmvcboot1/src/test/java/com/telusko/spmvc/Springmvcboot1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.telusko.spmvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springmvcboot1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
